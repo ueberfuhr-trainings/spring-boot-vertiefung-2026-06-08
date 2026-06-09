564db243ba232daaa26983d7dece21ed7d4c8b7f
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2026-06-09 16:44:54 +0200

    Optimize Domain Tests to exlude all auto-configurations exception validation. (#8)

--- src/test/java/de/schulung/spring/customers/domain/DomainTest.java
+++ src/test/java/de/schulung/spring/customers/domain/DomainTest.java
@@ -1,10 +1,17 @@
 package de.schulung.spring.customers.domain;
 
 import de.schulung.spring.customers.TestContextDefinition;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.boot.validation.autoconfigure.ValidationAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.event.RecordApplicationEvents;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -15,9 +22,10 @@ import java.lang.annotation.Target;
 /**
  * Domain Tests are still blackbox tests, but with mocked sink.
  */
-@SpringBootTest
+@BootstrapWith(SpringBootTestContextBootstrapper.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DomainTest.DomainTestConfiguration.class)
 @TestContextDefinition
-@AutoConfigureTestDatabase // TODO: disable other layers
 @MockitoBean(types = CustomersSink.class)
 @RecordApplicationEvents
 // Standard-Java-Annotations
@@ -25,4 +33,13 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Documented
 public @interface DomainTest {
+
+  @Configuration
+  @ComponentScan(basePackageClasses = DomainTest.class)
+  @ImportAutoConfiguration(classes = {
+    ValidationAutoConfiguration.class
+  })
+  class DomainTestConfiguration {
+  }
+
 }
