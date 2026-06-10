3c6a484eeabb592d4be0b81d6a8511e485af7ea1
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-06-10 14:17:26 +0200

    Implement `customers/{id}/address` endpoints

--- src/main/java/de/schulung/spring/customers/boundary/CustomerDtoMapper.java
+++ src/main/java/de/schulung/spring/customers/boundary/CustomerDtoMapper.java
@@ -3,10 +3,12 @@ package de.schulung.spring.customers.boundary;
 import de.schulung.spring.customers.domain.Customer;
 import de.schulung.spring.customers.domain.CustomerState;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerDtoMapper {
 
+  @Mapping(target = "address", ignore = true)
   Customer map(CustomerDto dto);
 
   default CustomerState mapState(String state) {
